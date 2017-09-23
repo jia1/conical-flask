9d8b54d5267fec716d23886630b7ee292e999b40
JIA YEE <jiayeerawr@gmail.com>
2017-09-22 18:28:39 -0700

    Add sort endpoint
    Signed-off-by: JIA YEE <jiayeerawr@gmail.com>

--- app.py
+++ app.py
@@ -1,7 +1,17 @@
-from flask import Flask
+from flask import Flask, jsonify, request
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return 'Hello, World!'
+
+@app.route('/sort', methods = ['POST'])
+def sorting():
+    if request.headers['Content-Type'] == 'application/json':
+        requestJson = request.get_json()
+        requestList = []
+        for key in requestJson:
+            requestList.append(requestJson[key])
+        return requestList
+    return 'NAK'
 
