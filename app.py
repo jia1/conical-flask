f1bf6023322ed6c660d76fa266ac0005eabc5682
JIA YEE <jiayeerawr@gmail.com>
2017-09-22 19:44:17 -0700

    Add heist endpoint
    Signed-off-by: JIA YEE <jiayeerawr@gmail.com>

--- app.py
+++ app.py
@@ -13,3 +13,36 @@ def sorting():
         return str(data)
     return 'NAK'
 
+@app.route('/heist', methods = ['POST'])
+def heist():
+    if request.headers['Content-Type'] == 'application/json':
+        data = request.json
+        maxWeight = data['maxWeight']
+        vault = data['vault']
+        listweights, listvalues = [], []
+        for item in vault:
+            listweights.append(item['weight'])
+            listvalues.append(item['value'])
+        count = 0
+        listratios = []
+        for i in listweights:
+            listratios.append(listvalues[count]/i)
+            count  = count + 1
+            sortedindices = [i[0] for i in sorted(enumerate(listratios), key=lambda x:x[1])];
+            listratios.sort();
+            listweights = [ listweights[i] for i in sortedindices]
+            weight = 0;
+            totalweight = 0;
+            count2= len(listratios)-1;
+            dollarvalue = 0;
+            while totalweight <= maxWeight:
+                reqratio = listratios[count2]
+                weight = listweights[count2]
+                prevtotal = totalweight
+                totalweight = totalweight + weight
+                if maxWeight < totalweight:
+                    dollarvalue = dollarvalue + (maxWeight - prevtotal)*reqratio;
+                    break
+                dollarvalue = dollarvalue + weight*reqratio;
+                count2 = count2 - 1;
+                return jsonify({"heist": dollarvalue});
