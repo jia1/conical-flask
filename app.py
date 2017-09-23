4903448c359f51d96f8e50de5353be652b2bf3e3
JIA YEE <jiayeerawr@gmail.com>
2017-09-22 20:49:12 -0700

    Add draft solution for stringcompression
    Signed-off-by: JIA YEE <jiayeerawr@gmail.com>

--- app.py
+++ app.py
@@ -50,9 +50,69 @@ def heist():
 
 @app.route('/stringcompression/<mode>', methods = ['POST'])
 def compress(mode):
+    answer = 0
     if request.headers['Content-Type'] == 'application/json':
-        pass
-    res = flask.Response('')
+        data = request.json['data']
+        if mode == 'RLE':
+            compressed = []
+            sequence = ['']
+            counter = {}
+            for c in data:
+                if c != sequence[-1]:
+                    counter[c] = 1
+                else:
+                    sequence.append(c)
+                    counter[c] += 1
+            for c in sequence:
+                compressed.append(counter[c])
+                compressed.append(c)
+            compressed = ''.join(compressed)
+            answer = len(answer) * 8
+        elif mode == 'LZW':
+            codes = []
+            lzw_dict = {}
+            next_code = 256
+            p = data[0]
+            for i in range(1, len(data)):
+                p_concat = p + data[i]
+                if (len(p_concat) == 1 and 0 <= ord(p_concat) <= 255) or p_concat in lzw_dict:
+                    p = p_concat
+                else:
+                    lzw_dict[p_concat] = next_code
+                    next_code += 1
+                    codes.append(p)
+                    p = data[i]
+            answer = len(codes) * 12
+        elif mode == 'WDE':
+            data = data.strip()
+            num_spaces = 0
+            num_non_alphanum = 0
+            prev = ''
+            lzw_dict = {}
+            next_code = 256
+            p = data[0]
+            for i in range(1, len(data)):
+                c = data[i]
+                if c == ' ':
+                    if prev != ' ':
+                        num_spaces += 1
+                elif not c.isalnum():
+                    num_non_alphanum += 1
+                else:
+                    p_concat = p + c
+                    if (len(p_concat) == 1 and 0 <= ord(p_concat) <= 255) or p_concat in lzw_dict:
+                        p = p_concat
+                    else:
+                        lzw_dict[p_concat] = next_code
+                        next_code += 1
+                    prev = c
+            if len(data) == 0:
+                answer = 0
+            else:
+                answer = (num_spaces + 1) * 12 + num_non_alphanum * 12 + len(lzw_dict)
+        else:
+            pass
+    res = flask.Response(str(answer))
     res.headers['Content-Type'] = 'text/plain'
     return res
 
