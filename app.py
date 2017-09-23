0d09e07d7b38daee5f5339723ce72584be1644bd
JIA YEE <jiayeerawr@gmail.com>
2017-09-23 01:54:15 -0700

    Add emptyarea endpoint
    Signed-off-by: JIA YEE <jiayeerawr@gmail.com>

--- app.py
+++ app.py
@@ -272,3 +272,103 @@ def plan():
                 list_max[prev_stn_name] = curr_count
 
         return jsonify({"line":nodes[curr_max_via].get_line(),"totalNumOfPassengers":curr_max,"reachingVia":curr_max_via})
+
+@app.route('/calculateemptyarea', methods = ['POST'])
+def empty():
+    if request.headers['Content-Type'] == 'application/json':
+        def rectOverlap(l1, r1 , l2, r2):
+            #If one rectangle is on left side of other
+            if (l1[0] > r2[0]) or (l2[0] > r1[0]):
+                return 0
+            #If one rectangle is above other
+            if (l1[1] < r2[1]) or (l2[1] < r1[1]):
+                return 0
+                #return 1;
+            if l1[0] < l2[0]:
+                if l1[1] >= l2[1]:
+                    return 1
+                else:
+                    return 2
+            else:
+                if r1[1] >= r2[1]:
+                    if r1[0]<= r2[0]:
+                        return 3
+                    else:
+                        return 4
+                else:
+                    return 5
+
+        data = request.data
+        container, child = {}, {}
+        for key in container:
+            if key == 'container':
+                container = data[key]
+            else:
+                child = data[key]
+
+        #container should be a list with first item coordinate, second item width
+        #and third item height
+        #child shape should be a list with first item shape, second item coordinate
+        #third item width/radius and fourth item height
+        childShape = child[0]
+        childCoord = child[1]
+        # childXCoord = childCoord[0];
+        #childYCoord = childCoord[1];
+        childWidth =child[2]
+        #outerchildWidth = childWidth + childXCoord;
+        #childHeight = child[3];
+        containerCoord = container[0]
+        #containerXCoord = containerCoord[0];
+        #containerycoord = containerCoord[1];
+        containerWidth = container[1]
+        containerHeight = container[2]
+        #outerContainerWidth = containerWidth + containerXCoord
+        #test cases:
+        containerCoord = [0,0]
+        childCoord = [5,2]
+        childWidth = 4
+        childHeight = 4
+        containerWidth = 10
+        containerHeight = 8
+        if (childShape == "rectangle") or (childShape=="square"):
+            childWidth = child[2]
+        if childShape=="rectangle":
+            childHeight = child[3]
+        else:
+            childHeight = childWidth;
+
+        l1 = [sum(x) for x in zip(childCoord, [0,childHeight])]
+        l2 = [sum(x) for x in zip(containerCoord, [0,containerHeight])]
+        r1 = [sum(x) for x in zip(childCoord, [childWidth,0])]
+        r2 = [sum(x) for x in zip(containerCoord, [containerWidth,0])]
+        containerArea = containerWidth*containerHeight
+
+        if rectOverlap(l1,r1,l2,r2) == 0:
+            emptyArea = containerArea
+        elif rectOverlap(l1,r1,l2,r2) == 1:
+            emptyArea = containerArea - (r1[0] - l2[0])*(l2[1] - r1[1])
+        elif rectOverlap(l1,r1,l2,r2) == 2:
+            if r2[1]<=r1[1]:
+                emptyArea = containerArea - (r1[1]-l1[1])*(r1[0]-l2[0])
+            else:
+                upperChildCor = [sum(x) for x in zip(childCoord, [childWidth,childHeight])]
+                emptyArea = containerArea - (upperChildCor[0]-containerCoord[0])*(upperChildCor[1]-containerCoord[1])
+        elif rectOverlap(l1,r1,l2,r2) == 3 :
+            if l1[1] <= l2[1]:
+                emptyArea = containerArea - (childWidth)*(childHeight)
+                #emptyArea = containerArea - (childWidth)*(l2[1]-r1[1]);
+            else:
+                #emptyArea = containerArea - (childWidth)*(childHeight);
+                emptyArea = containerArea - (childWidth)*(l2[1]-r1[1])
+        elif rectOverlap(l1,r1,l2,r2) == 4:
+            if l1[1] > l2[1]:
+                emptyArea = containerArea - (l2[1]-childCoord[1])*(r2[0]-childCoord[0])
+            else:
+                emptyArea = containerArea - (r2[0] - l1[0])*(childHeight)
+        else:
+            if r1[0]>r2[0]:
+                emptyArea = containerArea - (r2[0]-l1[0])*(r2[1]-l1[1])
+            else:
+                emptyArea = containerArea - childWidth*(l1[1]-r2[1])
+
+        return emptyArea
